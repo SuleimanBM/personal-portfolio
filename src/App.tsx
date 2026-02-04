c2f9b00f6def1d578422cf2d4042b5d338029714
Suleiman-baba <bmdanjumah@gmail.com>
2026-02-04 23:01:49 +0000

    removed baseroute from HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
 		<TooltipProvider>
 			<Toaster />
 			<Sonner />
-			<HashRouter basename="/personal-portfolio">
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Index />} />
 					{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
