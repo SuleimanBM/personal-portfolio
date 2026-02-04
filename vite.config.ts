c9a2b490dd59a571da1c5033d3b6fc9bd78dd039
Suleiman-baba <bmdanjumah@gmail.com>
2026-02-04 22:21:09 +0000

    add base to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/personal-portfolio/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
